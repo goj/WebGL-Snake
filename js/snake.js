d42506bc6368ed8be74014f8a5a5d53429c27c21
Krzysztof Goj <krzysztof.goj@gmail.com>
2011-11-26 17:51:05 +0100

    Square tail

--- js/snake.js
+++ js/snake.js
@@ -31,30 +31,34 @@ Snake.prototype = {
         this.spine.shift();
         this.spine.push([this.headX, this.headY]);
     },
+    appendRib: function(j, prevX, prevY, currX, currY) {
+        var dirX = currX - prevX;
+        var dirY = currY - prevY;
+        var dirLen = Math.sqrt(dirX*dirX + dirY*dirY);
+        dirX /= dirLen;
+        dirY /= dirLen;
+        this.contour[j++] = currX - dirY * SNAKE_WIDTH;
+        this.contour[j++] = currY + dirX * SNAKE_WIDTH;
+        this.contour[j++] = currX + dirY * SNAKE_WIDTH;
+        this.contour[j++] = currY - dirX * SNAKE_WIDTH;
+        return j;
+    },
     draw: function() {
         var ribsNo = Math.floor(this.spine.length / RIB_DIST);
-        var tailContourLength = 4*ribsNo-2;
-        var curX, curY, prevX, prevY, dirX, dirY;
+        var tailContourLength = 4*ribsNo;
+        var currX, currY, prevX, prevY;
 
-        var j=0;
-        prevX = this.contour[j++] = this.spine[0][0];
-        prevY = this.contour[j++] = this.spine[0][1];
+        var j = this.appendRib(0, 2*this.spine[0][0] - this.spine[1][0],
+                                  2*this.spine[0][1] - this.spine[1][1],
+                                  prevX = this.spine[0][0],
+                                  prevY = this.spine[0][1]);
+
         for (var i=RIB_DIST; i < this.spine.length; i += RIB_DIST) {
-            curX = this.spine[i][0];
-            curY = this.spine[i][1];
-            dirX = curX - prevX;
-            dirY = curY - prevY;
-            var dirLen = Math.sqrt(dirX*dirX + dirY*dirY);
-            dirX /= dirLen;
-            dirY /= dirLen;
+            j = this.appendRib(j, prevX, prevY,
+                                  currX = this.spine[i][0],
+                                  currY = this.spine[i][1]);
 
-            this.contour[j++] = curX - dirY * SNAKE_WIDTH;
-            this.contour[j++] = curY + dirX * SNAKE_WIDTH;
-            this.contour[j++] = curX + dirY * SNAKE_WIDTH;
-            this.contour[j++] = curY - dirX * SNAKE_WIDTH;
-
-            prevX = curX;
-            prevY = curY;
+            prevX = currX; prevY = currY;
         };
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vertPosBuf);
